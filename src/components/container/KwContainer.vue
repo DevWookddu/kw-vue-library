0c850264905fb3474dd2cc0c9ee2e6d21d52e6a1
DESKTOP-UKMQ0EM\JYW <devwookddu@gmail.com>
2022-06-01 02:15:32 +0900

    container

---
+++ src/components/container/KwContainer.vue
@@ -0,0 +1,342 @@
+<template>
+  <div
+    class="kw-container"
+    :class="[`flick-from-${flickFrom}`, { 'flick-move': isFlickMove }]"
+    :style="[elStyle, flickingPosition]"
+  >
+    <div v-if="$slots['header']" class="header" :class="{ shadow: useHeaderShadow && bodyScrollPosition }">
+      <slot name="header" />
+    </div>
+    <div class="body" ref="refBody">
+      <slot name="body" />
+      isMove: {{ isFlickMove }} / isFlicked : {{ isFlicked }} / flickStartX : {{ flickStartX }} / currentFlickX :
+      {{ currentFlickX }}
+    </div>
+    <div v-if="$slots['footer']" class="footer" :class="{ shadow: hasBodyScroll }">
+      <slot name="footer" />
+    </div>
+    <div ref="refContainers">
+      <slot name="containers" />
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { IdeComponent } from '@/decorators/IdeComponent';
+import { IdeProp } from '@/decorators/IdeProp';
+import { Component, Prop, Ref, Vue } from 'vue-property-decorator';
+
+function getXY(e: Event) {
+  if (window.TouchEvent && e instanceof TouchEvent) {
+    const [touchInfo] = e.changedTouches;
+    return {
+      x: touchInfo.clientX,
+      y: touchInfo.clientY,
+    };
+  } else {
+    const evt = e as MouseEvent;
+    return {
+      x: evt.screenX,
+      y: evt.screenY,
+    };
+  }
+}
+
+type LeftOrRight = 'left' | 'right' | false;
+type UpOrDown = 'up' | 'down' | false;
+
+@Component({
+  name: 'KwContainer',
+})
+@IdeComponent({ description: 'KwContainer Description' })
+export default class KwContainer extends Vue {
+  @Ref() private readonly refBody!: HTMLDivElement;
+  @Ref() private readonly refContainers!: { children: Array<{ __vue__: Vue }> };
+
+  @Prop({ type: Boolean })
+  @IdeProp({ type: 'boolean' })
+  private useHeaderShadow!: boolean;
+
+  @Prop({ type: Object })
+  private elStyle!: Record<string, unknown>;
+
+  @Prop({ type: String })
+  private flickFrom!: 'left' | 'bottom' | 'top' | 'right';
+
+  private bodyScrollDebounceId = 0;
+  private bodyScrollHeight = 0;
+  private bodyClientHeight = 0;
+  private bodyScrollPosition = 0;
+  private bodyResizeObserver: ResizeObserver | null = null;
+
+  private isFlickMove = false;
+  private flickStartTime = 0;
+  private flickStartX = 0;
+  private flickStartY = 0;
+  private isFlicked = false;
+  private currentFlickX = 0;
+  private currentFlickY = 0;
+
+  get flickingPosition() {
+    const { flickFrom, isFlickMove, isFlicked, currentFlickX, currentFlickY } = this;
+    if (!flickFrom) return null;
+
+    switch (flickFrom) {
+      case 'left': {
+        const flickX = currentFlickX === 0 ? '' : currentFlickX > 0 ? `+ ${currentFlickX}px` : `${currentFlickX}px`;
+        if (isFlickMove) {
+          return { transform: `translate(calc(${isFlicked ? '' : '-100%'} ${flickX}), 0)` };
+        } else {
+          return { transform: `translate(${isFlicked ? 0 : '-100%'}, 0)` };
+        }
+      }
+      case 'right':
+        return { transform: `translate(calc(100% + ${currentFlickX}px), 0)` };
+      case 'top':
+        return { transform: `translate(0, calc(100% + ${currentFlickY}px)` };
+      case 'bottom':
+        return { transform: `translate(0, calc(-100% + ${currentFlickY}px)` };
+    }
+
+    return null;
+  }
+
+  get hasBodyScroll() {
+    return this.bodyClientHeight < this.bodyScrollHeight;
+  }
+
+  mounted() {
+    this.setBodyHeights();
+    this.addBodyResizeObserver();
+    this.addFlickEventListener();
+    this.refBody.addEventListener('scroll', this.bodyScrollListener);
+  }
+
+  beforeDestroy() {
+    this.removeBodyResizeObserver();
+    this.removeFlickEventListener();
+    this.refBody.removeEventListener('scroll', this.bodyScrollListener);
+  }
+
+  private setBodyHeights() {
+    this.bodyScrollHeight = this.refBody?.scrollHeight || 0;
+    this.bodyClientHeight = this.refBody?.clientHeight || 0;
+  }
+
+  private addBodyResizeObserver() {
+    const bro = new ResizeObserver(() => {
+      this.setBodyHeights();
+    });
+    bro.observe(this.refBody);
+    this.bodyResizeObserver = bro;
+  }
+
+  private removeBodyResizeObserver() {
+    this.bodyResizeObserver?.disconnect();
+  }
+
+  private bodyScrollListener(e: Event) {
+    clearTimeout(this.bodyScrollDebounceId);
+    this.bodyScrollDebounceId = setTimeout(() => {
+      const { scrollTop, offsetHeight, scrollHeight } = e.target as HTMLDivElement;
+      const maxScroll = scrollHeight - offsetHeight;
+      const remainScroll = maxScroll - scrollTop;
+      const currentYPercent = scrollTop / maxScroll;
+      this.bodyScrollPosition = scrollTop;
+      this.$emit('scroll', {
+        scrollTop,
+        remainScroll,
+        currentYPercent,
+      });
+    });
+  }
+
+  private addFlickEventListener() {
+    this.$el.addEventListener('mousedown', this.elFlickStartListener);
+    this.$el.addEventListener('mousemove', this.elFlickMoveListener);
+    this.$el.addEventListener('mouseup', this.elFlickEndListener);
+    this.$el.addEventListener('touchstart', this.elFlickStartListener);
+    this.$el.addEventListener('touchmove', this.elFlickMoveListener);
+    this.$el.addEventListener('touchend', this.elFlickEndListener);
+  }
+
+  private removeFlickEventListener() {
+    this.$el.removeEventListener('mousedown', this.elFlickStartListener);
+    this.$el.removeEventListener('mousemove', this.elFlickMoveListener);
+    this.$el.removeEventListener('mouseup', this.elFlickEndListener);
+    this.$el.removeEventListener('touchstart', this.elFlickStartListener);
+    this.$el.removeEventListener('touchmove', this.elFlickMoveListener);
+    this.$el.removeEventListener('touchend', this.elFlickEndListener);
+  }
+
+  private elFlickStartListener(e: Event) {
+    const container = (e.target as HTMLElement).closest('.kw-container');
+    if (container === this.$el) {
+      const { x, y } = getXY(e);
+      this.setIsFlickMove(true);
+      this.flickStartX = x;
+      this.flickStartY = y;
+      this.flickStartTime = new Date().getTime();
+    }
+  }
+
+  public setIsFlickMove(move: boolean) {
+    this.isFlickMove = move;
+    this.getFlickContainers().forEach((node) => {
+      node.setIsFlickMove(move);
+    });
+  }
+
+  private getFlickContainers() {
+    const children = this.refContainers.children || [];
+    const containers = Array.from(children).filter(({ __vue__ }) => {
+      const { componentOptions } = __vue__.$vnode;
+      return (
+        componentOptions?.Ctor.name === 'KwContainer' &&
+        (componentOptions?.propsData as { flickFrom: string })?.flickFrom
+      );
+    });
+    return containers.map(({ __vue__ }) => __vue__) as Array<this>;
+  }
+
+  private elFlickMoveListener(e: Event) {
+    if (this.isFlickMove) {
+      const { x, y } = getXY(e);
+      if (this.isFlicked) {
+        this.currentFlickX = x - this.flickStartX;
+        this.currentFlickY = y - this.flickStartY;
+      } else {
+        this.getFlickContainers().forEach((node) => {
+          node.currentFlickX = x - this.flickStartX;
+          node.currentFlickY = y - this.flickStartY;
+        });
+      }
+    }
+  }
+
+  private elFlickEndListener(e: Event) {
+    this.setIsFlickMove(false);
+    const { innerHeight, innerWidth } = window;
+    const { x, y } = getXY(e);
+    let moveX = x - this.flickStartX;
+    let moveY = y - this.flickStartY;
+    const toRight = moveX > 0;
+    const toUp = moveY < 0;
+    moveX = Math.abs(moveX);
+    moveY = Math.abs(moveY);
+
+    const isSnap = new Date().getTime() - this.flickStartTime < 150;
+    let upOrDown: UpOrDown | false = false;
+    let leftOrRight: LeftOrRight = false;
+
+    if (isSnap) {
+      if (innerWidth / 8 < moveX || 52 < moveX) {
+        leftOrRight = toRight ? 'right' : 'left';
+      }
+      if (innerHeight / 8 < moveY || 52 < moveY) {
+        upOrDown = toUp ? 'up' : 'down';
+      }
+    } else {
+      if (innerWidth / 3 < moveX) {
+        leftOrRight = toRight ? 'right' : 'left';
+      }
+      if (innerHeight / 3 < moveY) {
+        upOrDown = toUp ? 'up' : 'down';
+      }
+    }
+
+    if (this.isFlicked) {
+      this.setFlickDataByEnd(leftOrRight, upOrDown, this);
+    } else {
+      this.getFlickContainers().forEach((node) => {
+        this.setFlickDataByEnd(leftOrRight, upOrDown, node);
+      });
+    }
+  }
+
+  private setFlickDataByEnd(leftOrRight: LeftOrRight, upOrDown: UpOrDown, node: this) {
+    switch (node.flickFrom) {
+      case 'left':
+        if (leftOrRight === 'right') {
+          node.isFlicked = true;
+        } else if (leftOrRight === 'left') {
+          node.isFlicked = false;
+        }
+    }
+    node.currentFlickX = 0;
+    node.currentFlickY = 0;
+  }
+}
+</script>
+
+<style scoped lang="scss">
+.kw-container {
+  position: fixed;
+  z-index: 2;
+  top: 0;
+  left: 0;
+  display: flex;
+  flex-direction: column;
+  width: 100vw;
+  height: 100%;
+  background-color: #fff;
+  user-select: none;
+  transition: transform 0.2s;
+
+  &.flick-move {
+    transition: none;
+  }
+
+  &.flick-from-top {
+    transform: translate(0, -100%);
+  }
+  &.flick-from-right {
+    transform: translate(100%, 0);
+  }
+  &.flick-from-bottom {
+    transform: translate(0, 100%);
+  }
+  &.flick-from-left {
+    transform: translate(-100%, 0);
+  }
+}
+
+.header {
+  position: relative;
+  z-index: 1;
+
+  &.shadow {
+    box-shadow: 0 0.2rem 0.4rem 0 rgba(0, 0, 0, 0.06);
+  }
+}
+
+.body {
+  box-sizing: border-box;
+  flex: 1;
+  overflow-y: auto;
+
+  @include for-ios {
+    padding-bottom: constant(safe-area-inset-bottom);
+    padding-bottom: env(safe-area-inset-bottom);
+  }
+
+  &.has-footer {
+    padding-bottom: 0;
+  }
+}
+
+.footer {
+  box-sizing: border-box;
+  position: relative;
+  z-index: 1;
+
+  &.shadow {
+    box-shadow: 0 0 0.4rem rgba(0, 0, 0, 0.08);
+  }
+
+  @include for-ios {
+    padding-bottom: calc(contant(safe-area-inset-bottom) - 2rem);
+    padding-bottom: calc(env(safe-area-inset-bottom) - 2rem);
+  }
+}
+</style>
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,16 @@ module.exports = {
   purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
+    screens: {
+      sm: '576px',
+      // => @media (min-width: 576px) { ... }
+
+      md: '960px',
+      // => @media (min-width: 960px) { ... }
+
+      lg: '1440px',
+      // => @media (min-width: 1440px) { ... }
+    },
     extend: {},
   },
   variants: {
